e4a993b1abad2c76327931446dca59efb68cc0f4
Pooca <87898039+Pooca1@users.noreply.github.com>
2022-03-14 09:06:47 +0100

    Update bg_menu.py

--- bg_menu.py
+++ bg_menu.py
@@ -3,29 +3,29 @@ import pygame
 class Background(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.mountain_bg = pygame.transform.scale(pygame.image.load("Assets/Mountains.png"), (800, 440))
-        self.forest_bg = pygame.transform.scale(pygame.image.load("Assets/Forest.png"), (800, 440))
-        self.sky_bg = pygame.transform.scale(pygame.image.load("Assets/SkyBG.png"), (800, 340))
-        self.forest_fg = pygame.transform.scale(pygame.image.load("Assets/pine1.png"), (800, 200))
-        self.forest_fg2 = pygame.transform.scale(pygame.image.load("Assets/pine2.png"), (800, 200))
-        self.boat = [pygame.transform.scale(pygame.image.load("Assets/Boat1.png"), (440, 180)),
-            pygame.transform.scale(pygame.image.load("Assets/Boat1.png"), (440, 180)),
-            pygame.transform.scale(pygame.image.load("Assets/Boat1.png"), (440, 180)),
-            pygame.transform.scale(pygame.image.load("Assets/Boat2.png"), (440, 180)),
-            pygame.transform.scale(pygame.image.load("Assets/Boat2.png"), (440, 180)),
-            pygame.transform.scale(pygame.image.load("Assets/Boat2.png"), (440, 180)),
-            pygame.transform.scale(pygame.image.load("Assets/Boat3.png"), (440, 180)),
-            pygame.transform.scale(pygame.image.load("Assets/Boat3.png"), (440, 180)),
-            pygame.transform.scale(pygame.image.load("Assets/Boat3.png"), (440, 180)),
-            pygame.transform.scale(pygame.image.load("Assets/Boat4.png"), (440, 180)),
-            pygame.transform.scale(pygame.image.load("Assets/Boat4.png"), (440, 180)),
-            pygame.transform.scale(pygame.image.load("Assets/Boat4.png"), (440, 180))]
-        self.nature_fg = [pygame.transform.scale(pygame.image.load("Assets/secondground1.png"), (80, 80)),
-                 pygame.transform.scale(pygame.image.load("Assets/secondground1.png"), (80, 80)),
-                 pygame.transform.scale(pygame.image.load("Assets/secondground1.png"), (80, 80)),
-                 pygame.transform.scale(pygame.image.load("Assets/secondground2.png"), (80, 80)),
-                 pygame.transform.scale(pygame.image.load("Assets/secondground2.png"), (80, 80)),
-                 pygame.transform.scale(pygame.image.load("Assets/secondground2.png"), (80, 80)),
-                 pygame.transform.scale(pygame.image.load("Assets/secondground3.png"), (80, 80)),
-                 pygame.transform.scale(pygame.image.load("Assets/secondground3.png"), (80, 80)),
-                 pygame.transform.scale(pygame.image.load("Assets/secondground3.png"), (80, 80))]
+        self.mountain_bg = pygame.transform.scale(pygame.image.load("Assets/Start_Menu/Mountains.png").convert_alpha(), (800, 440))
+        self.forest_bg = pygame.transform.scale(pygame.image.load("Assets/Start_Menu/Forest.png").convert_alpha(), (800, 440))
+        self.sky_bg = pygame.transform.scale(pygame.image.load("Assets/Start_Menu/SkyBG.png").convert_alpha(), (800, 340))
+        self.forest_fg = pygame.transform.scale(pygame.image.load("Assets/Start_Menu/pine1.png").convert_alpha(), (800, 200))
+        self.forest_fg2 = pygame.transform.scale(pygame.image.load("Assets/Start_Menu/pine2.png").convert_alpha(), (800, 200))
+        self.boat = [pygame.transform.scale(pygame.image.load("Assets/Start_Menu/Boat1.png").convert_alpha(), (440, 180)),
+            pygame.transform.scale(pygame.image.load("Assets/Start_Menu/Boat1.png").convert_alpha(), (440, 180)),
+            pygame.transform.scale(pygame.image.load("Assets/Start_Menu/Boat1.png").convert_alpha(), (440, 180)),
+            pygame.transform.scale(pygame.image.load("Assets/Start_Menu/Boat2.png").convert_alpha(), (440, 180)),
+            pygame.transform.scale(pygame.image.load("Assets/Start_Menu/Boat2.png").convert_alpha(), (440, 180)),
+            pygame.transform.scale(pygame.image.load("Assets/Start_Menu/Boat2.png").convert_alpha(), (440, 180)),
+            pygame.transform.scale(pygame.image.load("Assets/Start_Menu/Boat3.png").convert_alpha(), (440, 180)),
+            pygame.transform.scale(pygame.image.load("Assets/Start_Menu/Boat3.png").convert_alpha(), (440, 180)),
+            pygame.transform.scale(pygame.image.load("Assets/Start_Menu/Boat3.png").convert_alpha(), (440, 180)),
+            pygame.transform.scale(pygame.image.load("Assets/Start_Menu/Boat4.png").convert_alpha(), (440, 180)),
+            pygame.transform.scale(pygame.image.load("Assets/Start_Menu/Boat4.png").convert_alpha(), (440, 180)),
+            pygame.transform.scale(pygame.image.load("Assets/Start_Menu/Boat4.png").convert_alpha(), (440, 180))]
+        self.nature_fg = [pygame.transform.scale(pygame.image.load("Assets/Start_Menu/secondground1.png").convert_alpha(), (80, 80)),
+                 pygame.transform.scale(pygame.image.load("Assets/Start_Menu/secondground1.png").convert_alpha(), (80, 80)),
+                 pygame.transform.scale(pygame.image.load("Assets/Start_Menu/secondground1.png").convert_alpha(), (80, 80)),
+                 pygame.transform.scale(pygame.image.load("Assets/Start_Menu/secondground2.png").convert_alpha(), (80, 80)),
+                 pygame.transform.scale(pygame.image.load("Assets/Start_Menu/secondground2.png").convert_alpha(), (80, 80)),
+                 pygame.transform.scale(pygame.image.load("Assets/Start_Menu/secondground2.png").convert_alpha(), (80, 80)),
+                 pygame.transform.scale(pygame.image.load("Assets/Start_Menu/secondground3.png").convert_alpha(), (80, 80)),
+                 pygame.transform.scale(pygame.image.load("Assets/Start_Menu/secondground3.png").convert_alpha(), (80, 80)),
+                 pygame.transform.scale(pygame.image.load("Assets/Start_Menu/secondground3.png").convert_alpha(), (80, 80))]
